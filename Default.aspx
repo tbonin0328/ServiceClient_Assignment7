76f9e063aea64488631480672d62e01222b2a2d6
tbonin0328 <tbonin0328@yahoo.com>
2014-03-11 10:24:49 -0700

    First commit

---
+++ Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>
+        <asp:DataList ID="DataList1" runat="server">
+            <ItemTemplate>
+                <strong>
+                    <%#Eval("ServiceName") %>
+                   </strong> &nbsp;&nbsp;
+                    <%#Eval("ServiceDescription") %>
+                <br />
+            </ItemTemplate>
+        </asp:DataList>
+
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ServiceReference1.CommunityAssistServicesClient casc = new ServiceReference1.CommunityAssistServicesClient();
+
+        List<ServiceReference1.ComService> ourServices = new List<ServiceReference1.ComService>();
+        ourServices = casc.GetServices().ToList();
+
+
+        DataList1.DataSource = ourServices;
+        ////  foreach(ServiceReference1.ComService cs in ourServices)
+        //  {
+        //      DataList1.DataMember=cs.ServiceName;
+        //      DataList1.DataMember = cs.ServiceDescription;
+        //  }
+        DataList1.DataBind();
+
+    }
+}
